533438a19ab1d6dd43df622674aba0758bd85a78
xuzhiyong <>
2018-05-31 15:19:32 +0800

    bbbbb

--- src/main/java/com/ylz/cloud/mioroserviceprovideruser/MioroserviceProviderUserApplication.java
+++ src/main/java/com/ylz/cloud/mioroserviceprovideruser/MioroserviceProviderUserApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MioroserviceProviderUserApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("123456");
 		SpringApplication.run(MioroserviceProviderUserApplication.class, args);
+		System.out.println("abcdefg");
 	}
 }
